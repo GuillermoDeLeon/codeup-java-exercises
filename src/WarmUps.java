2287635fd4006c85c3cb0ffe6c971b47be41743f
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-03 13:18:39 -0500

    warmups exercise

---
+++ src/WarmUps.java
@@ -0,0 +1,15 @@
+public class WarmUps {
+
+    public static int[]  numberList = new int[] {10, 15, 3, 7};
+    public static int k = 17;
+
+    public static voice main(String[] args) {
+        private int k;
+        private int num1;
+        private int num2;
+
+    }
+
+
+}
+
